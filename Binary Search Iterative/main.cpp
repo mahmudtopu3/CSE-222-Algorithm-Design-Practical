35eafdfd877fe4ec66837d5df5b9c7c1a523ae07
mahmudtopu3 <mahmudtopu3@gmail.com>
2018-10-18 16:18:19 +0600

    Class #1 PART 1.1 ADDED

--- Binary Search Iterative/main.cpp
+++ Binary Search Iterative/main.cpp
@@ -1,9 +1,35 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
+// Class No.  #1 Part 1.1
+// C program to implement iterative Binary Search
+// By: Md. Mahmudul Huq Topu
+#include <stdio.h>
+#include <math.h>
+int binarySearch(int a[],int n,int x)
 {
-    cout << "Hello world!" << endl;
+   int low = 0,high = n,mid;
+   while(low<=high) {
+    mid = ceil((low+high)/2); /*if mid = 1.6 then it make mid = 2.0 nearest greater integer */
+    if(x<a[mid]){
+        high = mid-1;
+    }
+    else if(x>a[mid]) {
+        low = mid+1;
+    }
+    else {
+        return mid;
+    }
+
+   }
+   return 0;
+
+}
+
+int main(void)
+{
+    int arr[] = {2, 3, 4, 10, 40};
+    int n = sizeof(arr)/ sizeof(arr[0]);  /*sizeof(arr) returns the size taken by all of the elements which is (5*4)=20bytes*/
+    int x = 2;
+    int result = binarySearch(arr, n-1, x);
+    printf("%d is at position no: %d",x,result);
+
     return 0;
 }
